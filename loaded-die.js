2455127a75ee57c486c3a6bfedd75294c135e3e8
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-12 19:38:29 +0000

    Implemented loaded die closure.

--- loaded-die.js
+++ loaded-die.js
@@ -1,6 +1,12 @@
 var loadedDie = (function () {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  /* your code here */
+  var i = 0;
+
+  return function () {
+    i += 1;
+    return list[i-1];
+  }
+
 })();
 
 console.log(loadedDie());  // 5
